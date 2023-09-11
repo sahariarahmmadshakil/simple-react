1698c3d78f2d65e3976f9c2149a6fecfc2543793
sahariarahmmad <info.sashakil@gmail.com>
2023-09-11 15:09:27 +0600

    added update

--- src/App.jsx
+++ src/App.jsx
@@ -30,13 +30,13 @@ function App() {
   )
 }
 
-// function Town({ name, situated }) {
-//   return (
-//     <div>
-//       <h1> The Town Name Is: {name} </h1>
-//       <p>it is situtated near: <u>{situated}</u> it is really beautiful and also natural family </p>
-//     </div>
-//   )
-// }
+function Town({ name, situated }) {
+  return (
+    <div>
+      <h1> The Town Name Is: {name} </h1>
+      <p>it is situtated near: <u>{situated}</u> it is really beautiful and also natural family </p>
+    </div>
+  )
+}
 
 export default App
